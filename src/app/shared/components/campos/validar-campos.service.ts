d273897e8b20f1888f81266c063a827774c2d712
matheusosp <matheusosp@users.noreply.github.com>
2020-12-24 18:22:49 -0300

    initial commit

---
+++ src/app/shared/components/campos/validar-campos.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidarCamposService {
+
+  constructor() { }
+
+  hasErrorValidar(control: AbstractControl, errorName: string): boolean {
+    return (control.dirty || control.touched) && control.hasError(errorName);
+  }
+
+  lengthValidar(control: AbstractControl, errorName: string): number {
+    const error = control.errors[errorName];
+    return error.requiredLength || error.min || error.max || 0;
+  }
+}
